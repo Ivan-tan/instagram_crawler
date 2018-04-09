5aa348c5c14251d09d14d8a25889ea59d62bd49c
Ivan-tan <ivant.3211@gmail.com>
2018-04-09 14:53:15 +0800

    Add saving file with dir name

--- instagram_crawler.py
+++ instagram_crawler.py
@@ -3,7 +3,7 @@ import pandas as pd
 import datetime
 import sys
 
-def crawl(alias, limit):
+def crawl(alias, limit, dir_save):
     base_url = "https://www.instagram.com/%s/?__a=1" % alias
     url = base_url
 
@@ -11,8 +11,9 @@ def crawl(alias, limit):
     all_dfs = []
     r = requests.get(base_url)
     if r.json()["graphql"]["user"]["is_private"]:
-        print("Private account!")
-        sys.exit(0)
+        print("%s is a private account!" % alias)
+        return None
+    followers = r.json()["graphql"]["user"]["edge_followed_by"]["count"]
     while count <= limit:
         if r.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["page_info"]["has_next_page"]:
             max_id = r.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["page_info"]["end_cursor"]
@@ -24,15 +25,15 @@ def crawl(alias, limit):
             count += len(dfs)
 
     concat_df = pd.concat(all_dfs).reset_index(drop=True)
-
+    concat_df["followers"] = followers
     concat_df["caption"] = concat_df.edge_media_to_caption.apply(lambda x: x["edges"][0]["node"]["text"])
     concat_df["timestamp"] = pd.to_datetime(concat_df.taken_at_timestamp, unit="s")
     concat_df["likes"] = concat_df.edge_liked_by.apply(lambda x: x["count"])
     concat_df["comments"] = concat_df.edge_media_to_comment.apply(lambda x: x["count"])
 
-    clean_df = concat_df[["timestamp", "is_video", "caption", "display_url", "likes", "comments"]]
+    clean_df = concat_df[["timestamp", "is_video", "caption", "display_url", "likes", "comments", "followers"]]
 
-    clean_df.to_csv(alias+".csv", index=False)
+    clean_df.to_csv(dir_save+"/"+alias+".csv", index=False)
 
 def main():
     alias = sys.argv[1]
@@ -41,7 +42,8 @@ def main():
     except IndexError:
         limit = 100
     print("Extracting first %s posts for %s" % (limit, alias))
-    crawl(alias, limit)
+    dir_save = "./"
+    crawl(alias, limit, dir_save)
 
 if __name__ == "__main__":
     main()
